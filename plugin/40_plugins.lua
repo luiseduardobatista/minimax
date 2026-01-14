bbb4c688ea19e0cd11fa45001fa4bf3f6ab230fb
Luís Eduardo Batista <luisb@kyros.com.br>
2026-01-14 16:24:01 -0300

    feat(treesitter): improve treesitter cfg

--- plugin/40_plugins.lua
+++ plugin/40_plugins.lua
@@ -43,33 +43,39 @@ now_if_args(function()
     checkout = 'main',
   })
 
-  -- Define languages which will have parsers installed and auto enabled
-  local languages = {
-    -- These are already pre-installed with Neovim. Used as an example.
-    'lua',
-    'vimdoc',
-    'markdown',
-    -- Add here more languages with which you want to use tree-sitter
-    -- To see available languages:
-    -- - Execute `:=require('nvim-treesitter').get_available()`
-    -- - Visit 'SUPPORTED_LANGUAGES.md' file at
-    --   https://github.com/nvim-treesitter/nvim-treesitter/blob/main
-  }
-  local isnt_installed = function(lang)
-    return #vim.api.nvim_get_runtime_file('parser/' .. lang .. '.*', false) == 0
-  end
-  local to_install = vim.tbl_filter(isnt_installed, languages)
-  if #to_install > 0 then require('nvim-treesitter').install(to_install) end
-
-  -- Enable tree-sitter after opening a file for a target language
-  local filetypes = {}
-  for _, lang in ipairs(languages) do
-    for _, ft in ipairs(vim.treesitter.language.get_filetypes(lang)) do
-      table.insert(filetypes, ft)
-    end
-  end
-  local ts_start = function(ev) vim.treesitter.start(ev.buf) end
-  _G.Config.new_autocmd('FileType', filetypes, ts_start, 'Start tree-sitter')
+  require('nvim-treesitter.configs').setup({
+    ensure_installed = {
+      'lua',
+      'vimdoc',
+      'markdown',
+      'python',
+      'go',
+      'rust',
+      'html',
+      'css',
+      'json',
+      'dockerfile',
+    },
+    auto_install = true,
+    highlight = {
+      enable = true,
+    },
+    indent = {
+      enable = true,
+    },
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true,
+        keymaps = {
+          ['af'] = '@function.outer',
+          ['if'] = '@function.inner',
+          ['ac'] = '@class.outer',
+          ['ic'] = '@class.inner',
+        },
+      },
+    },
+  })
 end)
 
 -- Language servers ===========================================================
