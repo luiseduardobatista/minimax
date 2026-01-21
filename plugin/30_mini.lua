9df9c50a209fc1cb9f34adc8a167461a7a13b29d
Luís Eduardo Batista <luiseduardob303@gmail.com>
2026-01-21 16:31:23 -0300

    feat(mini): add bind to send items from mini.pick to quickfix list

--- plugin/30_mini.lua
+++ plugin/30_mini.lua
@@ -697,6 +697,10 @@ later(function()
       col = math.floor(0.5 * (vim.o.columns - width)),
     }
   end
+  local choose_all = function()
+    local mappings = MiniPick.get_picker_opts().mappings
+    vim.api.nvim_input(mappings.mark_all .. mappings.choose_marked)
+  end
   require('mini.pick').setup({
     window = {
       config = win_config,
@@ -712,6 +716,7 @@ later(function()
           )
         end,
       },
+      send_to_quickfix = { char = '<C-q>', func = choose_all },
     },
   })
 end)
